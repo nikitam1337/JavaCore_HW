a548a659be75ce82fae7d63ed00cb90bb021e5a0
nikitam1337 <nikitam.l@mail.ru>
2023-09-09 21:50:37 +0300

    Day 1 compleated

--- HW_4/program/src/main/java/ru/geekbrains/MyRandomArray.java
+++ HW_4/program/src/main/java/ru/geekbrains/MyRandomArray.java
@@ -1,4 +1,27 @@
 package ru.geekbrains;
+import java.util.Random;
+public class MyRandomArray extends MyArray {
 
-public class MyRandomArray {
+    private static Random random = new Random();
+    private static int sizeX;
+    private static int sizeY;
+
+    /**
+     * Создает новый массив
+     *
+     * @param array Передаваемый двумерный массив строк
+     * @param sizeX размер по горизонтали
+     * @param sizeY размер по вертикали
+     */
+    public MyRandomArray() {
+        super(array, sizeX, sizeY);
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    int sizeX = random.nextInt(2,6);
+    int sizeY = random.nextInt(2,6);
 }
